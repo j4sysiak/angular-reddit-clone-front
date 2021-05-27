743890a2bd5b96b89f6a1d15b73b4a5eb416293a
Jacek <jaceksysiak@wp.pl>
2021-05-27 07:46:01 +0200

    27-05-2021 07:45

---
+++ src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-header/ng4-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng4-header',
+  templateUrl: './ng4-header.component.html',
+  styleUrls: ['./ng4-header.component.css']
+})
+export class Ng4HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-recipes/ng4-recipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng4-recipes',
+  templateUrl: './ng4-recipes.component.html',
+  styleUrls: ['./ng4-recipes.component.css']
+})
+export class Ng4RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
