e5c25862821e1b3cc37d779f2c93a9b3db7b169a
Jacek <jaceksysiak@wp.pl>
2021-10-21 06:51:52 +0200

    21-10-2021 06:51

--- src/app/UdemyAngularTheCompleteGuide/cmp-databinding/server-element/server-element.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/cmp-databinding/server-element/server-element.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'] //, encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
