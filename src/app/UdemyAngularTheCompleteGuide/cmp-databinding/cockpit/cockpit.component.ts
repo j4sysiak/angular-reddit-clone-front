c7f33e9413dd89f83bee73def4d7d6c0629e65f5
Jacek <jaceksysiak@wp.pl>
2021-10-22 08:14:45 +0200

    22-10-2021 08:13

--- src/app/UdemyAngularTheCompleteGuide/cmp-databinding/cockpit/cockpit.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/cmp-databinding/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,9 +7,11 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // tslint:disable-next-line:no-output-rename
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: true}) serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -17,19 +19,17 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer(nameInput: HTMLInputElement) {
-    // console.log(nameInput);
+    console.log(this.serverContentInput);
     this.serverCreated.emit({
-      // serverName: this.newServerName,
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      // serverName: this.newServerName,
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
--- src/app/UdemyAngularTheCompleteGuide/cmp-databinding/server-element/server-element.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/cmp-databinding/server-element/server-element.component.ts
@@ -3,9 +3,10 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css'] //, encapsulation: ViewEncapsulation.None
+  styleUrls: ['./server-element.component.css'] // , encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
+  // tslint:disable-next-line:no-input-rename
   @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
