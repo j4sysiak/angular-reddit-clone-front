8f6f061fa0e068b514a366cf268a287da84cd28e
Jacek <jaceksysiak@wp.pl>
2021-11-04 07:17:46 +0100

    04-11-2021 06:23

--- src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-recipes/recipe.service.ts
+++ src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Ng4Recipe} from './ng4-recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,21 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Ng4Recipe>();
 
   private recipes: Ng4Recipe[] = [
-    new Ng4Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Ng4Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Ng4Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Ng4Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   constructor() {
