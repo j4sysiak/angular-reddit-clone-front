cafeed7b897f87203c02120e9b7c6673e2fd2d78
Jacek <jaceksysiak@wp.pl>
2021-11-08 06:54:30 +0100

    08-11-2021 06:24

--- src/app/UdemyAngularTheCompleteGuide/assignment-routing/servers-routing/servers-routing.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/assignment-routing/servers-routing/servers-routing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ServersRoutingService} from './servers-routing.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-servers-routing',
@@ -10,10 +11,13 @@ export class ServersRoutingComponent implements OnInit {
 
   public serversRouting: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversRoutingService: ServersRoutingService) { }
+  constructor(private serversRoutingService: ServersRoutingService, private router: Router) { }
 
   ngOnInit() {
     this.serversRouting = this.serversRoutingService.getServersRouting();
   }
 
+  onReload() {
+    this.router.navigate(['/servers-routing']);
+  }
 }
