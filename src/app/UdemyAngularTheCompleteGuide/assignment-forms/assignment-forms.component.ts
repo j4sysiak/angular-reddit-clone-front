423e38bd6776c4e6aff9075a7aef68039d40ad1f
Jacek <jaceksysiak@wp.pl>
2021-11-22 08:16:19 +0100

    22-11-2021 07:08

--- src/app/UdemyAngularTheCompleteGuide/assignment-forms/assignment-forms.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/assignment-forms/assignment-forms.component.ts
@@ -11,9 +11,31 @@ export class AssignmentFormsComponent implements OnInit {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'templete@wp.pl'
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   constructor() { }
@@ -24,7 +46,14 @@ export class AssignmentFormsComponent implements OnInit {
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // }
+
   onSubmit() {
     console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email    = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
