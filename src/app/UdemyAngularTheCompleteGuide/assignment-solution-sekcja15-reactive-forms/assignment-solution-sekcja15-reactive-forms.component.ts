2a21d108334e72eaed1ae7d0a9cb5b1af1c6af62
Jacek <jaceksysiak@wp.pl>
2021-11-30 07:46:03 +0100

    30-11-2021 07:36

--- src/app/UdemyAngularTheCompleteGuide/assignment-solution-sekcja15-reactive-forms/assignment-solution-sekcja15-reactive-forms.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/assignment-solution-sekcja15-reactive-forms/assignment-solution-sekcja15-reactive-forms.component.ts
@@ -9,13 +9,14 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 export class AssignmentSolutionSekcja15ReactiveFormsComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   constructor() { }
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -29,10 +30,17 @@ export class AssignmentSolutionSekcja15ReactiveFormsComponent implements OnInit 
 
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
-    ( <FormArray> this.signupForm.get('hobbies')).push(control);
+    (<FormArray> this.signupForm.get('hobbies')).push(control);
   }
 
   getControls() {
     return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  forbiddenNames(control: FormControl):  {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
