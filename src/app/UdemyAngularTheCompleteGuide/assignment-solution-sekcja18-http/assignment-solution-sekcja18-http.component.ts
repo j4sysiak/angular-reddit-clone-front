3d2d12c5068a66550917b65d0e38d74e1f3f5988
Jacek <jaceksysiak@wp.pl>
2021-12-13 07:20:57 +0100

    13-12-2021 06:08

--- src/app/UdemyAngularTheCompleteGuide/assignment-solution-sekcja18-http/assignment-solution-sekcja18-http.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/assignment-solution-sekcja18-http/assignment-solution-sekcja18-http.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators'
+import {Post} from './post.model';
 
 @Component({
   selector: 'app-assignment-solution-sekcja18-http',
@@ -11,20 +13,43 @@ export class AssignmentSolutionSekcja18HttpComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     // console.log(postData);
-    this.http.post('https://ng4-complete-guide-13-12-2021-default-rtdb.firebaseio.com/posts.json', postData);
+    this.http.post<{name: string}>('https://ng4-complete-guide-13-12-2021-default-rtdb.firebaseio.com/posts.json', postData)
+      .subscribe(responseData => {
+        // console.log(responseData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get<{ [key: string]: Post }>('https://ng4-complete-guide-13-12-2021-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+        //...
+        console.log(posts);
+      });
   }
 
 }
