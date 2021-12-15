93d191af58d228a1af84431ccd5441ef938b6a0f
Jacek <jaceksysiak@wp.pl>
2021-12-15 07:07:28 +0100

    15-12-2021 06:48

--- src/app/UdemyAngularTheCompleteGuide/assignment-solution-sekcja18-http/assignment-solution-sekcja18-http.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/assignment-solution-sekcja18-http/assignment-solution-sekcja18-http.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators'
 import {Post} from './post.model';
+import {PostsService} from './posts.service';
 
 @Component({
   selector: 'app-assignment-solution-sekcja18-http',
@@ -12,47 +13,24 @@ export class AssignmentSolutionSekcja18HttpComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    // console.log(postData);
-    this.http.post<{ name: string }>('https://ng4-complete-guide-13-12-2021-default-rtdb.firebaseio.com/posts.json', postData)
-      .subscribe(responseData => {
-        // console.log(responseData);
-      });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http.get<{ [key: string]: Post }>('https://ng4-complete-guide-13-12-2021-default-rtdb.firebaseio.com/posts.json')
-      .pipe(map(responseData => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postsArray.push({ ...responseData[key], id: key });
-          }
-        }
-        return postsArray;
-      }))
-      .subscribe(posts => {
-        console.log(posts);
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      });
   }
 
 }
