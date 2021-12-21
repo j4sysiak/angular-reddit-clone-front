fe9b4399490339e748f3a1a4a7e9ec49661be358
Jacek <jaceksysiak@wp.pl>
2021-12-21 13:52:51 +0100

    21-12-2021 12:29

--- src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-recipes/recipes-resolver.service.ts
+++ src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-recipes/recipes-resolver.service.ts
@@ -3,15 +3,22 @@ import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/rou
 import {Ng4Recipe} from './ng4-recipe.model';
 import {Observable} from 'rxjs';
 import {DataStorageService} from '../shared/data-storage.service';
+import {RecipeService} from './recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolverService implements Resolve<Ng4Recipe[]> {
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Ng4Recipe[]> | Promise<Ng4Recipe[]> | Ng4Recipe[] {
-    return  this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
