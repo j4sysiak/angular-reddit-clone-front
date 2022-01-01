e73906119f39704a6ad81f635cb156599ba52726
Jacek <jaceksysiak@wp.pl>
2022-01-01 10:16:38 +0100

    01-01-2022 08:57

--- src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-auth/ng4-auth.component.ts
+++ src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-auth/ng4-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {Ng4AuthService} from './ng4-auth.service';
+import {AuthResponseData, Ng4AuthService} from './ng4-auth.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-ng4-auth',
@@ -25,22 +26,27 @@ export class Ng4AuthComponent   {
     const email = form.value.email;
     const password = form.value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
+
     if (this.isLoginMode) {
-      // ...
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        resData => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        errorMessage => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signup(email, password);
     }
+
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
 
     form.reset();
   }
--- src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-auth/ng4-auth.service.ts
+++ src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-auth/ng4-auth.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   // kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -29,17 +30,77 @@ export class Ng4AuthService {
           returnSecureToken: true
         }
       ).pipe(
-        catchError(errorRes => {
-          let errorMessage = 'An unknown error occurred!';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email exists already';
-          }
-          return throwError(errorMessage);
-        })
+        catchError(this.handleError)
       );
   }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDUdRDLzUDw0IKRN7S9Xy43yvJtJpnDQoo',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+    }
+    return throwError(errorMessage);
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
