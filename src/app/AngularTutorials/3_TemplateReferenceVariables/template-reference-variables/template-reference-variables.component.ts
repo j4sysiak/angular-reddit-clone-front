a5794a96de832b1325731016acbea3715cb1b671
Jacek <jaceksysiak@wp.pl>
2022-01-18 06:54:45 +0100

    18-01-2022 06:04

---
+++ src/app/AngularTutorials/3_TemplateReferenceVariables/template-reference-variables/template-reference-variables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-reference-variables',
+  templateUrl: './template-reference-variables.component.html',
+  styleUrls: ['./template-reference-variables.component.css']
+})
+export class TemplateReferenceVariablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
