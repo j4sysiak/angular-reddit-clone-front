719978c0c50b3ec7a0df8fa202488fa2573c0db6
Jacek <jaceksysiak@wp.pl>
2022-01-23 08:20:20 +0100

    23-01-2022 06:16

---
+++ src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-recipes/recipe.service.ts
@@ -0,0 +1,40 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ng4Recipe} from './ng4-recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../ng4-shopping-list/shopping-list.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Ng4Recipe>();
+
+  private recipes: Ng4Recipe[] = [
+    new Ng4Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Ng4Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
---
+++ src/app/UdemyAngularTheCompleteGuide/ng4-complete-guide/ng4-shopping-list/shopping-list.service.ts
@@ -0,0 +1,36 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingListService {
+
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] =  [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  constructor() {}
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    //dupa
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+}
